8dd5cf138da3310300564b991f4b62f51e528e38
Kristian Coulson <kristian.coulson@prettylittlething.com>
2021-03-25 08:25:31 +0000

    update util

--- utils/__tests__/add.test.js
+++ utils/__tests__/add.test.js
@@ -1,5 +1,5 @@
 const { add } = require('../add')
 
 it('should add', () => {
-  expect(add(1, 3)).toBe(4);
+  expect(add(1, 3)).toBe(1);
 })
--- utils/add.js
+++ utils/add.js
@@ -1,3 +1,6 @@
-const add = (a, b) => a + b;
+const add = (a, b, should) => {
+  if (!should) return a;
+  return a + b
+};
 
 module.exports = { add }
